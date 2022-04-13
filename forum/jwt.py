5bbec965b7664cb65d2bef231f2dba7d13030f2b
pavlekovacevic <kovacevicpavle14@gmail.com>
2022-04-13 14:46:53 +0200

    fifth commit

--- forum/jwt.py
+++ forum/jwt.py
@@ -6,8 +6,8 @@ from forum import db
 from forum.models import User
 from forum.config import ForumConfig as config
 
-def token_required(func):
-    @wraps(func)
+def token_required(f):
+    @wraps(f)
     def decorated(*args, **kwargs):
         
         token = request.headers.get("Authorization")
@@ -18,16 +18,17 @@ def token_required(func):
         
         try:
             # decoding the payload to fetch the stored details
-            import pdb;pdb.set_trace()
-            token_payload = jwt.decode(jwt=token, key=config.SECRET_KEY)
+            pure_token=token[7:]
+            token_payload = jwt.decode(jwt=pure_token, key=config.SECRET_KEY, algorithms=['HS256'])
             current_user = User.query.filter_by(id = token_payload['user_id']).first()
         
         except Exception as e:
+            import pdb;pdb.set_trace()
             print(e)
             return jsonify({
                 'message' : 'Token is invalid !!'
             }), 401
         # returns the current logged in users contex to the routes
-        return  func(current_user, *args, **kwargs)
+        return  f(current_user, *args, **kwargs)
   
     return decorated
--- forum/routes.py
+++ forum/routes.py
@@ -9,6 +9,7 @@ from forum import db
 from forum.schemas import CreateCommentInputSchema, CreatePostInputSchema, CreateUserInputSchema, CreateLoginInputSchema
 from forum.config import ForumConfig as config
 from forum.jwt import token_required
+
 users_blueprint = Blueprint('users', __name__, url_prefix='/users')
 blog_blueprint = Blueprint('blog', __name__, url_prefix='/blog')
 
@@ -52,7 +53,7 @@ def login():
 
 @blog_blueprint.route('/post', methods=['POST'])
 @token_required
-def create_post():
+def create_post(self):
     try:
         post_data = CreatePostInputSchema().load(request.get_json())
     except ValidationError as err:
@@ -60,78 +61,14 @@ def create_post():
 
     # token_required vraca ulogovani user, sad samo proveriti kako da pristupas toj promenljivoj
     new_post = Post(title=post_data['title'], content = post_data['content'])
-
+    
     db.session.add(new_post)
     db.session.commit()
 
     post_id = getattr(new_post, 'id')
     return jsonify({'id':post_id}), 201
 
-@blog_blueprint.route('/post', methods=['GET'])
-@token_required
-def get_all_posts():
-    posts = Post.query.all()
-    all_posts = []
-    for post in posts:
-        all_posts.append(post)
-    
-    return jsonify({'all posts': all_posts})
 
-@blog_blueprint.route('/post/<id>', methods=['GET'])
-@token_required
-def get_single_post_by_id(id):
-    post = Post.query.filter_by(id=id).first()
-    if not post:
-        return 'No matching post_id', 404
-
-    return jsonify(post)
-
-@blog_blueprint.route('/post/<id>', methods = ['PUT'])
-@token_required
-def update_post(id):
-    try:
-        post_data = CreatePostInputSchema().load(request.get_json())
-    except ValidationError as err:
-        return err.messages, 400
-    
-    post = Post.query.filter_by(id=id).first()
-
-    post.title = post_data['title']
-    post.content = post_data['content']
-    
-    updated_post = post
-
-    db.session.commit()
-    return jsonify(updated_post)
-
-@blog_blueprint.route('/post', methods=['DELETE'])
-@token_required
-def delete_post_by_id(id):
-    post = Post.query.filter_by().first()
-    db.session.delete(post)
-    db.session.commit()
-
-    return jsonify("Deleted"), 200
-
-@blog_blueprint.route('/post/<post_id>/comment/<comment_id>', methods=['GET'])
-@token_required
-def add_comment_to_post_by_id(post_id, comment_id):
-    """"patch comment by seraching its id and post id"""
-
-@blog_blueprint.route('/comment', methods=['GET'])
-def get_all_comments():
-    try:
-        comment_data = CreateCommentInputSchema().load(request.get_json)
-    except ValidationError as err:
-        return err.messages, 400
-
-    new_comment = Comment(content=comment_data['content'])
-
-    db.session.add(new_comment)
-    db.session.commit()
-
-    comm_id = getattr(new_comment, 'id')
-    return jsonify({'id':comm_id})
 
 
 
